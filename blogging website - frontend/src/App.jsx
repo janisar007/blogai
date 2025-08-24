6ca4fafc05b808f9e28487bdffe5e9752c918a3c
janisar007 <134937141+janisar007@users.noreply.github.com>
2025-08-24 19:36:05 +0530

    404 page done

--- blogging website - frontend/src/App.jsx
+++ blogging website - frontend/src/App.jsx
@@ -6,6 +6,7 @@ import { lookInSession } from "./common/session";
 import Editor from "./pages/editor.pages";
 import Homepage from "./pages/home.page";
 import SearchPage from "./pages/search.page";
+import PageNotFound from "./pages/404.page";
 
 export const UserContext = createContext({});
 
@@ -39,6 +40,7 @@ const App = () => {
           <Route path="signup" element={<UserAuthForm type={"sign-up"} />} />
 
           <Route path="search/:query" element={<SearchPage />} />
+          <Route path="*" element={<PageNotFound />} />
         </Route>
       </Routes>
     </UserContext.Provider>
