b69fb7851e45c55edfdaea232e2dbb433423cff4
janisar007 <134937141+janisar007@users.noreply.github.com>
2025-08-24 21:35:06 +0530

    bare minimum user profile completed

--- blogging website - frontend/src/App.jsx
+++ blogging website - frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Editor from "./pages/editor.pages";
 import Homepage from "./pages/home.page";
 import SearchPage from "./pages/search.page";
 import PageNotFound from "./pages/404.page";
+import ProfilePage from "./pages/profile.page";
 
 export const UserContext = createContext({});
 
@@ -40,6 +41,8 @@ const App = () => {
           <Route path="signup" element={<UserAuthForm type={"sign-up"} />} />
 
           <Route path="search/:query" element={<SearchPage />} />
+          <Route path="user/:id" element={<ProfilePage />} />
+
           <Route path="*" element={<PageNotFound />} />
         </Route>
       </Routes>
--- server/Routers/user.router.js
+++ server/Routers/user.router.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import {  searchUsers } from "../Controllers/user.controller.js";
+import {  searchUsers, getProfile } from "../Controllers/user.controller.js";
 import { verifyJWT } from "../Middlewares/verifyJWT.middleware.js";
 const router = Router();
 
 
 router.route("/search-users").post(searchUsers);
+router.route("/get-profile").post(getProfile);
 
 export default router;
